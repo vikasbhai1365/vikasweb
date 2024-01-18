ec210e5ca585c011afd2cd274bf3791720f5c00f
vikasbhai1365 <157020801+vikasbhai1365@users.noreply.github.com>
2024-01-18 17:54:55 +0530

    add myweb.html

---
+++ myweb.js
@@ -0,0 +1,5 @@
+let projBtn=document.querySelector(".projectBtn");
+let projectContainer=document.querySelector(".project-container");
+projBtn.addEventListener("click",()=>{
+    projectContainer.style.display="block";
+})
